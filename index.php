8bb88a1a093f41a6f7d4c6a3a6a098aa7e8cc9bd
vinyex <irvinepramudya@gmail.com>
2018-08-11 13:57:45 +0700

    Fixing add Todo

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 <?php
 	// initialize errors variable
-		$errors = "";
+	$errors = "";
 		
 	// connect to database
 	$db = mysqli_connect("localhost", "root", "", "todo");
 	
 	//insert a quote if submit button is clicked
-	if (isset($_post['submit'])) {
-		if (empty($_post['task'])) {
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
 			$errors = "You must fill in the task.";
 		}else {
-			$task = $_post['task'];
+			$task = $_POST['task'];
 			$sql = "insert into tasks (task) values ('$task')";
 			mysqli_query($db, $sql);
 			header('location: index.php');
@@ -29,7 +29,6 @@
 		<div class="heading">
 			<h2 style="font-style: 'Hervetica';">ToDo List Application PHP and MySql Database</h2>
 		</div>
-		
 		<form method="post" action="index.php" class="input_form">
 			<input type="text" name="task" class="task_input">
 			<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
@@ -49,7 +48,9 @@
 		// select all tasks if page is visited or refreshed
 		$tasks = mysqli_query($db, "SELECT * FROM tasks");
 
-		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+		$i = 1; 
+
+		while ($row = mysqli_fetch_array($tasks)) { ?>
 			<tr>
 				<td> <?php echo $i; ?> </td>
 				<td class="task"> <?php echo $row['task']; ?> </td>
